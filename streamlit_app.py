6332c19a350aba070a1ef46a92c7847e5eb7d523
Nehil singhal <112778892+nehils2022@users.noreply.github.com>
2022-09-11 10:20:43 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,7 +27,7 @@ try:
      streamlit.dataframe(fruityvice_normalized)
 except urlerror as e:
   streamlit.error()
- streamlit.stop()
+streamlit.stop()
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
